c75465ecb18cb3c82d85333b62e03da88bcc3e72
Oskar <Zaden>
2024-09-09 17:56:22 +0200

    Creating MainAccountPage

---
+++ src/pages/MainAccountPage/MainAccountPage.jsx
@@ -0,0 +1,13 @@
+
+// importing styles
+import "./MainAccountPage.css"
+
+export const MainAccountPage = () => {
+
+
+    return (
+        <>
+            <h1 className="display-3 my-auto fw-bold">Welcome to <br/> Crow Chat</h1>
+        </>
+    )
+}
