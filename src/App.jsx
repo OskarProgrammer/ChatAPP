764095abd7969591b128644ab9228501962a1d98
Oskar <Zaden>
2024-09-09 15:01:31 +0200

    Preparing project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// importing styles
+import './App.css'
+
+// importing bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
