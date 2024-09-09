2568aadf490d050f641269bf9a9391643265d40c
Oskar <Zaden>
2024-09-09 16:53:05 +0200

    Creating form and action to login process

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,16 @@
 
 // importing styles
+import { getUserByName } from "../../api_functions/getRequests"
+import { putRequest } from "../../api_functions/putRequests"
 import "./LoginPage.css"
 
 // importing functions and components from react library
-import { Form, Link } from "react-router-dom"
+import { Form, Link, redirect, useActionData } from "react-router-dom"
 
 export const LoginPage = () => {
 
+    // getting actionData
+    const actionData = useActionData()
 
     return (
         <>
@@ -15,10 +19,11 @@ export const LoginPage = () => {
                 Login Form
             </h2>
 
-            <Form method="POST" className="d-flex flex-column gap-3 pt-3">
+            <Form method="POST" action="/login" className="d-flex flex-column gap-3 pt-3">
 
                 <input  type="text" 
                         placeholder="Login"
+                        name="login"
                         className=" text-center
                                     p-2
                                     fs-4
@@ -29,6 +34,7 @@ export const LoginPage = () => {
 
                 <input  type="password" 
                         placeholder="Password"
+                        name="password"
                         className=" text-center
                                     p-2
                                     fs-4
@@ -37,7 +43,9 @@ export const LoginPage = () => {
                                     rounded
                                     border-0 shadow"/>
                 
-                <p className="">Haven't got account yet ? <Link to="/register">Click here</Link> </p>
+                <span className="">Haven't got account yet ? <Link to="/register">Click here</Link> </span>
+                
+                {actionData && actionData.error && <span className="text-danger" >{actionData.error}</span>}
 
                 <button className=" btn 
                                     btn-outline-success 
@@ -49,3 +57,37 @@ export const LoginPage = () => {
     )
 }
 
+export const loginAction = async ( { request } ) => {
+    
+    // getting formData
+    const formData = await request.formData()
+
+    // getting form fields
+    const login = formData.get('login')
+    const password = formData.get("password")
+
+    // validating data
+    if ( login == "" || password == "" ) {
+        return { error : "All fields must be provided" }
+    } 
+
+    const user = await getUserByName(login)
+
+    if ( !user ) {
+        return { error : "This account doesn't exist"}
+    }
+
+    // creatint newCurrent object
+    const newCurrent = {
+        id : user.id,
+        isLogged : true
+    }
+
+    try {
+        await putRequest("http://localhost:3000/currentUser/", newCurrent)
+    } catch {
+        return { error : "Something went wrong during logging"}
+    }
+
+    return redirect("/account/")
+}
