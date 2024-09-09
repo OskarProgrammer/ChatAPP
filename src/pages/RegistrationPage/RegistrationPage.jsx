60040a10a18391bb88e18ae23886e095847036b3
Oskar <Zaden>
2024-09-09 16:38:09 +0200

    Creating form and action for that form

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -2,10 +2,17 @@
 // importing styles
 import "./RegistrationPage.css"
 
+// importing api functions
+import { postRequest } from "../../api_functions/postRequests"
+import { putRequest } from "../../api_functions/putRequests"
+
 // importing functions and components from react library
-import { Form, Link } from "react-router-dom"
+import { Form, Link, redirect, useActionData } from "react-router-dom"
+import { getUserByName } from "../../api_functions/getRequests"
 
 export const RegistrationPage = () => {
+    // getting actionData
+    const actionData = useActionData() 
 
 
     return(
@@ -19,6 +26,7 @@ export const RegistrationPage = () => {
 
                 <input  type="text" 
                         placeholder="Login"
+                        name="login"
                         className=" text-center
                                     p-2
                                     fs-4
@@ -29,6 +37,7 @@ export const RegistrationPage = () => {
 
                 <input  type="password" 
                         placeholder="Password"
+                        name="password"
                         className=" text-center
                                     p-2
                                     fs-4
@@ -39,6 +48,7 @@ export const RegistrationPage = () => {
 
                 <input  type="password" 
                         placeholder="Confirm password"
+                        name="confirmPassword"
                         className=" text-center
                                     p-2
                                     fs-4
@@ -47,7 +57,9 @@ export const RegistrationPage = () => {
                                     rounded
                                     border-0 shadow"/>
                 
-                <p className="">Have got account already ? <Link to="/login">Click here</Link> </p>
+                <span className="">Have got account already ? <Link to="/login">Click here</Link> </span>
+
+                {actionData && actionData.error && <span className="text-danger" >{actionData.error}</span>}
 
                 <button className=" btn 
                                     btn-outline-success 
@@ -59,6 +71,47 @@ export const RegistrationPage = () => {
     )
 }
 
-export const registerAction = async () => {
-    
+export const registerAction = async ( {request} ) => {
+    // getting form data
+    const formData = await request.formData()
+
+    // getting fields from form
+    const login = formData.get("login")
+    const password = formData.get("password")
+    const confirmPassword = formData.get("confirmPassword")
+
+    // validating data
+    if (login == "" || password == "" || confirmPassword == "") {
+        return { error : "All fields must be provided"}
+    }
+
+    if (password != confirmPassword) {
+        return { error : "Password and confirm password must be the same"}
+    }
+
+    if (!getUserByName(login)) {
+        return { error : "This login is taken already"}
+    }
+
+    // creating newUser object
+    const newUser = {
+        id : crypto.randomUUID(),
+        login : login,
+        password : password
+    }
+
+    try {
+        await postRequest(`http://localhost:3000/users/`,newUser)
+    } catch {
+        return { error : "Something went wrong during creating new user"}
+    }
+
+    // logging into account
+    try {
+        await putRequest("http://localhost:3000/currentUser/", { id : newUser.id, isLogged: true})
+    } catch {
+        return { error : "Something went wrong during logging into account"}
+    }
+
+    return redirect("/")
 }
