9877513a3455b1510a47fffea3036c9378fc2cf1
Oskar <Zaden>
2024-09-09 21:43:20 +0200

    Creating useEffect to fetch info about owner of the message

--- src/components/MessageTab/MessageTab.jsx
+++ src/components/MessageTab/MessageTab.jsx
@@ -2,15 +2,42 @@
 // importing styles
 import "./MessageTab.css"
 
+// importing api functions
+import { getRequest } from "../../api_functions/getRequests"
+
+// importing functions and components from react library
+import { useEffect, useState } from "react"
+
+
 export const MessageTab = (props) => {
 
     // getting props data
     const message = props.messageInfo
     const currentUser = props.currentUserInfo
 
+    // creating useState variables
+    let [ownerInfo, setOwnerInfo] = useState({})
+
+    // useEffect to fetch data about owner of message
+    useEffect(()=>{
+        const timeout = setTimeout( async () => {
+            ownerInfo = await getRequest(`http://localhost:3000/users/${message.ownerID}`)
+            setOwnerInfo(ownerInfo)
+        },1)
+
+
+        return () => ( clearTimeout(timeout) )
+    })
+
     return (
-        <div className={`container-fluid d-flex ${message.ownerID == currentUser.id ? "justify-content-end" : "justify-content-start"}`}>
-            <span className={`fs-5 p-3 rounded ${message.ownerID == currentUser.id ? "bg-primary text-light" : "bg-dark text-light"}`}>{message.message}</span>
+        <div className={`container-fluid d-flex flex-row ${message.ownerID == currentUser.id ? "justify-content-end" : "justify-content-start"}`}>
+
+            {message.ownerID == currentUser.id ? 
+                <span className={`fs-5 p-3 rounded bg-primary text-light message`}>{message.message}</span>
+                :
+                <span className={`fs-5 p-3 rounded bg-dark text-light message`}>{ownerInfo.login} : {message.message}</span>
+            }   
+
         </div>
     )
 }
