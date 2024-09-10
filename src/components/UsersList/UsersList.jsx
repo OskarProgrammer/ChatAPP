90e1857ec20a40bd6b8c5eefe36c72a79870ed9a
Oskar <Zaden>
2024-09-10 18:52:47 +0200

    Creating UsersList

---
+++ src/components/UsersList/UsersList.jsx
@@ -0,0 +1,49 @@
+
+// importing functions and components from react library
+import { useEffect, useState } from "react"
+
+// importing api functions
+import { getCurrentUser, getRequest } from "../../api_functions/getRequests"
+
+
+export const UsersList = (props) => {
+
+    // creating useState variables
+    let [items, setItems] = useState([])
+
+    // useEffect fucntion to fetch all users
+    useEffect(()=>{
+        const timeout = setTimeout( async () => {
+            // getting currentUser
+            const currentUser = await getCurrentUser()
+
+            // getting all users
+            const users = await getRequest(`http://localhost:3000/users/`)
+
+            // filtring
+            items = users.filter(e => (e.id != currentUser.id) && (!props.chatPeople.includes(e.id)) )
+
+            items.map( async (item) => {
+                const user = await getRequest(`http://localhost:3000/users/${item}`)
+                item = {
+                    id : item,
+                    login : user.login
+                }
+            })
+
+            setItems(items)
+
+        },100)
+
+        return () => { clearTimeout(timeout) }
+    })
+
+
+    return(
+        <>
+            {items.map((item) => (
+                <option value={item.login} />
+            ))}
+        </>
+    )
+}
