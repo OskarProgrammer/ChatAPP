7933f235294e18a1bc90d470bf903ba2a7cffab6
Oskar <Zaden>
2024-09-10 19:26:07 +0200

    Creating action function newChatAction

--- src/pages/NewChatPage/NewChatPage.jsx
+++ src/pages/NewChatPage/NewChatPage.jsx
@@ -1,12 +1,15 @@
 
 // importing functions and components from react library
 import { useState } from "react"
-import { Form } from "react-router-dom"
+import { Form, redirect } from "react-router-dom"
 
 // importing components
-import { getCurrentUser, getUserByName } from "../../api_functions/getRequests"
 import { UserTab } from "../UserTab/UserTab"
 import { UsersList } from "../../components/UsersList/UsersList"
+
+// importing api functions
+import { getCurrentUser, getUserByName } from "../../api_functions/getRequests"
+import { postRequest } from "../../api_functions/postRequests"
 
 
 export const NewChatPage = () => {
@@ -46,13 +49,17 @@ export const NewChatPage = () => {
             <h2 className="display-3">New chat</h2>
 
             <Form method="POST" action="/account/createChat" className="d-flex flex-column gap-3 shadow p-5">
-                
-                <input type="text" placeholder="Name of chat" className="col-6 p-3 fs-5 text-center mx-auto"/>
+                <input value={chatPeople} name="groupOfPeople" hidden readOnly/>
+
+                <input  type="text"
+                        name="title"
+                        placeholder="Name of chat" 
+                        className="col-lg-6 col-12 p-3 fs-5 text-center mx-auto"/>
 
                 <input type="text"  placeholder="Add user" 
                                     value={newUser} 
                                     onChange={(e)=>{setNewUser(e.target.value)}} 
-                                    className="col-6 p-3 fs-5 text-center mx-auto"
+                                    className="col-lg-6 col-12 p-3 fs-5 text-center mx-auto"
                                     list="users"/>
 
                 <datalist id="users" >
@@ -60,7 +67,7 @@ export const NewChatPage = () => {
                 </datalist>
 
 
-                <button type="button" onClick={()=>{ addUser() }} className="btn btn-outline-success btn-lg col-3 mx-auto">
+                <button type="button" onClick={()=>{ addUser() }} className="btn btn-outline-success btn-lg col-lg-3 col-6 mx-auto">
                     <i class="bi bi-plus fs-2" />
                 </button>
 
@@ -70,15 +77,56 @@ export const NewChatPage = () => {
                 <div className="container-fluid usersContainer row d-flex justify-content-center">
 
                     {chatPeople.map((person)=>{
-                        return (
-                            <UserTab personID={person}
-                                     onRemove={removeUser}/>
-                        )
+                        try {
+                            return (
+                                <UserTab personID={person}
+                                         onRemove={removeUser}/>
+                            )
+                        } catch { return }
                     })}
 
                 </div>
 
+                <button className="btn btn-outline-success col-lg-6 col-12 mx-auto fs-3">
+                    Create
+                </button>
+
             </Form>
         </>
     )
+}
+
+export const newChatAction = async ( {request} ) =>{
+    // getting formdata
+    const formData = await request.formData()
+
+    // getting currentUser
+    const currentUser = await getCurrentUser()
+
+    // getting input fields
+    const name = formData.get("title")
+    const groupOfPeople = formData.get("groupOfPeople")
+
+    // validating data
+    if ( name == "" || groupOfPeople.length == 0 ) { return }
+
+    // creating newChat object
+    const newChatID = crypto.randomUUID()
+
+    const newChat = {
+        id : newChatID,
+        name : name,
+        participants: [groupOfPeople, currentUser.id],
+        lastMessage: ""
+      }
+
+    // posting data to db
+    try {
+        await postRequest(`http://localhost:3000/chats/`,newChat)
+    } catch {
+        throw new Error("Something went wrong during creating new chat")
+    }
+
+    return redirect(`/account/chat/${newChatID}`)
+
 }
