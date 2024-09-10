efeac6166639223fb0fbec6b9cb99e8b9e71a8ce
Oskar <Zaden>
2024-09-10 21:06:26 +0200

    Styling code

--- src/components/MessageTab/MessageTab.jsx
+++ src/components/MessageTab/MessageTab.jsx
@@ -8,6 +8,9 @@ import { getRequest } from "../../api_functions/getRequests"
 // importing functions and components from react library
 import { useEffect, useState } from "react"
 
+// importing date functions
+import { getHoursDiff, getMinutesDiff, getSecondsDiff } from "../../date_functions/date_functions"
+
 
 export const MessageTab = (props) => {
 
@@ -17,6 +20,7 @@ export const MessageTab = (props) => {
 
     // creating useState variables
     let [ownerInfo, setOwnerInfo] = useState({})
+    let [isExpanded, setIsExpanded] = useState(false)
 
     // useEffect to fetch data about owner of message
     useEffect(()=>{
@@ -29,14 +33,50 @@ export const MessageTab = (props) => {
         return () => ( clearTimeout(timeout) )
     })
 
-    return (
-        <div className={`container-fluid d-flex flex-row ${message.ownerID == currentUser.id ? "justify-content-end" : "justify-content-start"}`}>
+    const diff = () =>{
+        // creating format
+        const responseFormat = ( text ) =>{
+            return "Sent " + text + " ago"
+        }
 
-            {message.ownerID == currentUser.id ? 
-                <span className={`fs-5 p-3 rounded bg-primary text-light message`}>{message.message}</span>
-                :
-                <span className={`fs-5 p-3 rounded bg-dark text-light message`}>{ownerInfo.login} : {message.message}</span>
-            }   
+        // creating current date
+        const currentDate = new Date()
+
+        // getting diff between current date and date of creating the message
+        const secondsDiff = getSecondsDiff(currentDate, new Date(message.createdAt))
+        const minutesDiff = getMinutesDiff(currentDate, new Date(message.createdAt))
+        const hoursDiff = getHoursDiff(currentDate, new Date(message.createdAt))
+        
+        if ( secondsDiff > 60 ){
+            if ( minutesDiff > 60) {
+                return responseFormat(`${hoursDiff} hours`)
+            }else{
+                return responseFormat(`${minutesDiff} minutes`)
+            }
+        } else {
+            return responseFormat(`${secondsDiff} seconds`)
+        }
+
+    }
+
+    // creating format of timeMessage
+    const timeOfSending = isExpanded ? <span className="text-center">{ diff() }</span> : ""
+
+    // creating format of message
+    const messageFormat = message.ownerID == currentUser.id ? <span className={`fs-5 p-3 rounded bg-primary text-light message`}>{message.message}</span> :
+                                                              <span className={`fs-5 p-3 rounded bg-dark text-light message`}>{ownerInfo.login} : {message.message}</span>
+    
+    // creating sideOfMessage
+    const sideOfMessage = message.ownerID == currentUser.id ? "justify-content-end" : "justify-content-start"
+
+    return (
+        <div className={`container-fluid d-flex flex-row ${sideOfMessage}`}
+             onClick={()=> { setIsExpanded(!isExpanded)} }>
+
+                <div className="col-lg-3 d-flex flex-column">
+                    {messageFormat}
+                    {timeOfSending}
+                </div>
 
         </div>
     )
