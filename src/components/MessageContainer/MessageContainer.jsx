23b1d925ea26277a4a9b042504139dff3e46b42b
Oskar <Zaden>
2024-09-11 15:58:26 +0200

    Modifying MessageContainer

--- src/components/MessageContainer/MessageContainer.jsx
+++ src/components/MessageContainer/MessageContainer.jsx
@@ -1,6 +1,16 @@
+
+// importing functions and components from react library
+import { useEffect, useState } from "react"
+
+
+// importing components
 import { HeaderOfConversation } from "../HeaderOfConversation/HeaderOfConversation"
 import { MessageTab } from "../MessageTab/MessageTab"
-import { PersonTab } from "../PersonTab/PersonTab"
+
+
+// importing styles
+import "./MessageContainer.css"
+
 
 export const MessageContainer = (props) => {
 
@@ -8,11 +18,15 @@ export const MessageContainer = (props) => {
     const messages = props.messages
     const chatInfo = props.chatInfo
     const currentUser = props.currentUser
+    const currentResponse = props.currentResponse
+    
 
     return(
         <div className="container-fluid 
                             d-flex flex-column flex-column-reverse
-                            text-dark chatContainer shadow py-3 gap-3">
+                            text-dark chatContainer shadow py-3 gap-3 position-relative">
+                
+                
 
                 {messages.map((message)=>(
 
@@ -25,6 +39,13 @@ export const MessageContainer = (props) => {
                 <HeaderOfConversation chatInfo={chatInfo} 
                                       currentUser={currentUser}/>
 
+                {/* {currentResponse.id != "" ? 
+                    <span className="p-3 fw-bold bg-primary rounded position-sticky text-light d-flex flex-row justify-content-around response">
+                        <p className="bg-primary py-3 my-auto p-3 rounded">You are responsing to : <br/> {currentResponse.message}</p>
+                        <button className="btn btn-outline-danger btn-lg">Stop responsing</button>
+                    </span> 
+                : ""} */}
+                
         </div>
     )
 }
